85bb196aef31153272624e198640c67b617bd788
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-06-24 12:15:11 +0900

    refactor: move BuildTaskDtos method

--- src/TaskSchedulerManager/Program.cs
+++ src/TaskSchedulerManager/Program.cs
@@ -3,20 +3,7 @@
     static void Main(string[] args)
     {
         PrivilegeManager.EnsureAsAdmin(args); // need admin right for editing admin's task
-        var taskDtos = BuildTaskDtos("tasks.yaml");
+        var taskDtos = TaskDto.BuildTaskDtos("tasks.yaml");
         TaskSchedulerManager.SyncTasks(taskDtos, "MyTasks");
-    }
-
-    public static List<TaskDto> BuildTaskDtos(string yamlPath)
-    {
-        var deserializer = new DeserializerBuilder()
-            .WithNamingConvention(UnderscoredNamingConvention.Instance)
-            .Build();
-        var yamlText = File.ReadAllText(yamlPath);
-        var taskInputs = deserializer.Deserialize<List<TaskInput>>(yamlText);
-
-        return taskInputs
-            .Select(input => new TaskDto(input, TaskBuilder.BuildTask(input)))
-            .ToList();
     }
 }
--- src/TaskSchedulerManager/Task/TaskDto.cs
+++ src/TaskSchedulerManager/Task/TaskDto.cs
@@ -1,9 +1,13 @@
 using Microsoft.Win32.TaskScheduler;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 public class TaskDto
 {
     public TaskInput Input { get; set; }
     public TaskDefinition Definition { get; set; }
+
+    public string TaskPath => $@"MyTasks\{Input.Triggers[0].Type}\{Input.Name}";
 
     public TaskDto(TaskInput input, TaskDefinition definition)
     {
@@ -11,5 +15,16 @@ public class TaskDto
         Definition = definition;
     }
 
-    public string TaskPath => $@"MyTasks\{Input.Triggers[0].Type}\{Input.Name}";
+    public static List<TaskDto> BuildTaskDtos(string yamlPath)
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(UnderscoredNamingConvention.Instance)
+            .Build();
+        var yamlText = File.ReadAllText(yamlPath);
+        var taskInputs = deserializer.Deserialize<List<TaskInput>>(yamlText);
+
+        return taskInputs
+            .Select(input => new TaskDto(input, TaskBuilder.BuildTask(input)))
+            .ToList();
+    }
 }
